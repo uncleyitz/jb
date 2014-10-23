d03ebcd5db2fd7a9b3e2646ba8a56988064ebb3e
uncleyitz <uncleyitz@gmail.com>
2014-10-23 11:40:38 +0300

    Create targil.js

---
+++ targil.js
@@ -0,0 +1,16 @@
+function enterPressed(){
+	var screenValue = document.getElementById("screen").value;
+	if (screenValue.trim().length  == 0) {
+		return;
+	}
+	document.getElementById("screen").value = '';
+	var historyList = document.querySelector(".historyFrame > ul");
+	var newLi = document.createElement("li");
+	newLi.innerHTML = escape(screenValue);
+	historyList.appendChild(newLi);
+}
+
+function clicked(text) {
+	document.getElementById("screen").value += text;
+}
+
